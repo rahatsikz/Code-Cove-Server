a11801927ff25d0b7a6c1610f3d5f71ac28b36a8
rahatsikz <rascal6271@gmail.com>
2023-01-26 21:46:41 +0600

    course data added

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require("./data/courses.json");
+
 app.get("/", (req, res) => {
   res.send("Code cove server running!");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
