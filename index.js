52905ed8a2cb20a681a3288c538a4e9bd770d98f
rahatsikz <rascal6271@gmail.com>
2023-01-27 22:54:16 +0600

    details data created

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require("./data/courses.json");
+const details = require("./data/details.json");
 
 app.get("/", (req, res) => {
   res.send("Code cove server running!");
@@ -14,6 +15,12 @@ app.get("/", (req, res) => {
 
 app.get("/courses", (req, res) => {
   res.send(courses);
+});
+
+app.get("/details/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = details.find((course) => course.id === id);
+  res.send(selectedCourse);
 });
 
 app.listen(port, () => {
